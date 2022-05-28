9d390c0b2ee07afc22268243f5e2986301add1c0
Felix Otoo <ofelix03@gmail.com>
2022-05-28 23:47:39 +0000

    add a new stream processor

--- src/main/java/transformations/stateful/WindowedStream.java
+++ src/main/java/transformations/stateful/WindowedStream.java
@@ -7,23 +7,24 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.*;
 
+import java.awt.event.WindowEvent;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * A streams processing application that takes a source stream, group and aggregate streams.
+ * A streams processing application that takes a source stream, group, windows and aggregates.
  * */
-public class GroupByKeyStream {
+public class WindowedStream {
 
-    public static String INPUT_TOPIC = "groupby-key-stream-input";
-    public static String OUTPUT_TOPIC = "groupby-key-stream-output";
+    public static String INPUT_TOPIC = "windowed-stream-input";
+    public static String OUTPUT_TOPIC = "windowed-stream-output";
 
     public static void main(String[] args) {
 
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "groupby-key-stream");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "windowed-stream");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
@@ -33,13 +34,17 @@ public class GroupByKeyStream {
         source.flatMapValues((key, value) -> Arrays.asList(value.split("\\s+")));
         source.peek((key, value) -> System.out.println(String.format("Peek:: %s - %s", key, value)));
 
-        KTable<String, Long> table = source.flatMapValues((key, value) -> Arrays.asList(value.split("\\s+")))
+        KTable<Windowed<String>, Long> table = source.flatMapValues((key, value) -> Arrays.asList(value.split("\\s+")))
                 .groupBy((key, value) -> value)
-                        .count();
+                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofSeconds(10)))
+                .count(Materialized.as("windowed-counts"));
+
+        final Serde<Windowed<String>> windowedSerde = WindowedSerdes.timeWindowedSerdeFrom(String.class, 10);
+
 
         table.toStream()
                 .peek((key, value) -> System.out.println(String.format("After aggregation:: %s - %s", key, value)))
-                .to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.Long()));
+                .to(OUTPUT_TOPIC, Produced.with(windowedSerde, Serdes.Long()));
 
         final KafkaStreams streams = new KafkaStreams(builder.build(), props);
         final CountDownLatch latch = new CountDownLatch(1);
--- src/main/java/transformations/stateful/WordCountDemo.java
+++ src/main/java/transformations/stateful/WordCountDemo.java
@@ -1,4 +1,5 @@
-package transformations.stateless.stateful;/*
+package transformations.stateful;
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
